e53f5474a1de2bb7c227213c9aefe3f520b98147
Tom Bartlett <tom.bartlett@assetinsights.co.uk>
2023-12-22 19:12:16 +0000

    init

---
+++ apps/Pi/src/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:5000");
+  } else {
+    mainWindow.loadFile("./index.html");
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
